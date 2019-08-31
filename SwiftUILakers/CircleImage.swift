54713be5c0dbc9c1a3b759161c49d74cdb91ed1c
Sumit Joshi <sumitjoshi@Sumits-MacBook-Pro.local>
2019-08-31 11:07:50 +0900

    Added the circle image of an organizatino logo

---
+++ SwiftUILakers/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  SwiftUILakers
+//
+//  Created by Sumit Joshi on 2019/08/31.
+//  Copyright © 2019 Sumit Joshi. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("lakers")
+        .clipShape(Circle())
+            .overlay(Circle().stroke(Color.purple, lineWidth: 15))
+        .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
