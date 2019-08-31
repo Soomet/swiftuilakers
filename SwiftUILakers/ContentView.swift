98e855b05861e080b40f634be88e221311984dfa
Sumit Joshi <sumitjoshi@Sumits-MacBook-Pro.local>
2019-08-31 10:25:31 +0900

    First commit

---
+++ SwiftUILakers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUILakers
+//
+//  Created by Sumit Joshi on 2019/08/31.
+//  Copyright © 2019 Sumit Joshi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello Universe!!!! wassuo?")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
