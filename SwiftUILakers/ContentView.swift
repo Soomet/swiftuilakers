14779acf1bae7f181e7f92295d6443bcc5250ac3
Sumit Joshi <sumitjoshi@Sumits-MacBook-Pro.local>
2019-08-31 11:21:35 +0900

    Set all the components(map, logo, organization name and player name) together

--- SwiftUILakers/ContentView.swift
+++ SwiftUILakers/ContentView.swift
@@ -10,34 +10,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("LA Lakers")
-                .font(.title)
-                .foregroundColor(Color.yellow)
-            HStack {
-                Text("Black Mamba")
-                    .font(.subheadline)
-                    .foregroundColor(Color.purple)
-                Spacer()
-                Text("Shaq")
-                    .font(.subheadline)
-                    .foregroundColor(Color.purple)
-                Spacer()
-                Text("Fisher")
-                    .font(.subheadline)
-                    .foregroundColor(Color.purple)
-                Spacer()
-                Text("Pau Gasol")
-                    .font(.subheadline)
-                    .foregroundColor(Color.purple)
-                Spacer()
-                Text("Malone")
-                    .font(.subheadline)
-                    .foregroundColor(Color.purple)
-                Spacer()
+        VStack {
+            ArenaAddressView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("LA Lakers")
+                    .font(.title)
+                    .foregroundColor(Color.yellow)
+                HStack {
+                    Text("Black Mamba")
+                        .font(.subheadline)
+                        .foregroundColor(Color.purple)
+                    Spacer()
+                    Text("Shaq")
+                        .font(.subheadline)
+                        .foregroundColor(Color.purple)
+                    Spacer()
+                    Text("Fisher")
+                        .font(.subheadline)
+                        .foregroundColor(Color.purple)
+                    Spacer()
+                    Text("Pau Gasol")
+                        .font(.subheadline)
+                        .foregroundColor(Color.purple)
+                    Spacer()
+                    Text("Malone")
+                        .font(.subheadline)
+                        .foregroundColor(Color.purple)
+                }
             }
+            .padding()
+            Spacer()
         }
-    .padding()
     }
 }
 
